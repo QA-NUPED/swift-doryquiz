d6c112b3e22d7bad52c1214e5bdeb5fff023a394
jhonyso <jhonysds2@gmail.com>
2023-09-11 10:38:48 -0300

    test

--- Scripts/ci-ios-code-coverage.sh
+++ Scripts/ci-ios-code-coverage.sh
@@ -15,37 +15,37 @@ fi
 set -o pipefail && env NSUnbufferedIO=YES xcodebuild build-for-testing -project "doryQuiz/doryQuiz.xcodeproj" -scheme "doryQuizTests" -destination "platform=iOS Simulator,OS=latest,name=iPhone 14" -enableCodeCoverage YES | xcpretty
 # Test
 set -o pipefail && env NSUnbufferedIO=YES xcodebuild test-without-building -project "doryQuiz/doryQuiz.xcodeproj" -scheme "doryQuizTests" -destination "platform=iOS Simulator,OS=latest,name=iPhone 14" -enableCodeCoverage YES -resultBundlePath $RESULT_BUNDLE | xcpretty
-#Report
-# set -o pipefail && env NSUnbufferedIO=YES xcodebuild -project "doryQuiz/doryQuiz.xcodeproj" -scheme "doryQuizTests" build "COMPILER_INDEX_STORE_ENABLE=NO" test -destination "id=B6C47C9F-927E-4B99-B86A-D8DE5A8B84E1" -resultBundlePath "/var/folders/6q/wgy6jtp12w5gzgm9lzcglpqw0000gn/T/XCUITestOutput001844682/Test.xcresult" "GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES" "GCC_GENERATE_TEST_COVERAGE_FILES=YES" | xcpretty "--color" "--report" "html" "--output" "swift-doryquiz/CodeCoverage.xcresult/Info.plist"
-# Converter o formato de cobertura gerado pelo Xcode para o formato LCOV
-# xcrun llvm-cov export -format="lcov" -instr-profile "doryQuiz/doryQuiz.xcodeproj/CodeCoverage.xcresult/doryQuizTests/doryQuiz.app" -o "CodeCoverage.xcresult/coverage.lcov"
-# plconvert -gcov Info.plist
-# gcovr -r ./CodeCoverage.xcresult -g -k -o output.lcov
+# #Report
+# # set -o pipefail && env NSUnbufferedIO=YES xcodebuild -project "doryQuiz/doryQuiz.xcodeproj" -scheme "doryQuizTests" build "COMPILER_INDEX_STORE_ENABLE=NO" test -destination "id=B6C47C9F-927E-4B99-B86A-D8DE5A8B84E1" -resultBundlePath "/var/folders/6q/wgy6jtp12w5gzgm9lzcglpqw0000gn/T/XCUITestOutput001844682/Test.xcresult" "GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES" "GCC_GENERATE_TEST_COVERAGE_FILES=YES" | xcpretty "--color" "--report" "html" "--output" "swift-doryquiz/CodeCoverage.xcresult/Info.plist"
+# # Converter o formato de cobertura gerado pelo Xcode para o formato LCOV
+# # xcrun llvm-cov export -format="lcov" -instr-profile "doryQuiz/doryQuiz.xcodeproj/CodeCoverage.xcresult/doryQuizTests/doryQuiz.app" -o "CodeCoverage.xcresult/coverage.lcov"
+# # plconvert -gcov Info.plist
+# # gcovr -r ./CodeCoverage.xcresult -g -k -o output.lcov
 
-set -o pipefail && env NSUnbufferedIO=YES xcrun xccov view --report --json $RESULT_BUNDLE > $RESULT_JSON
-# Nome do arquivo de saída LCOV
-lcov_file="${$RESULT_JSON}.lcov"
+# set -o pipefail && env NSUnbufferedIO=YES xcrun xccov view --report --json $RESULT_BUNDLE > $RESULT_JSON
+# # Nome do arquivo de saída LCOV
+# lcov_file="${$RESULT_JSON}.lcov"
 
-# Crie um arquivo LCOV vazio
-echo "TN:" > "$lcov_file"
+# # Crie um arquivo LCOV vazio
+# echo "TN:" > "$lcov_file"
 
-cat "$RESULT_JSON" | jq -r '.[] | "SF:\(.sourceFile)" as $sf | .lines | keys[] as $line | "DA:\($line),\(.[$line])"' >> "$lcov_file"
+# cat "$RESULT_JSON" | jq -r '.[] | "SF:\(.sourceFile)" as $sf | .lines | keys[] as $line | "DA:\($line),\(.[$line])"' >> "$lcov_file"
 
-# Adicione o marcador de final de registro
-echo "end_of_record" >> "$lcov_file"
+# # Adicione o marcador de final de registro
+# echo "end_of_record" >> "$lcov_file"
 
-echo "Conversão concluída. O arquivo LCOV foi salvo como: $lcov_file"
-#Filtrando para que a variavel code coverage apenas contenha a cobertura total
-CODE_COVERAGE=$(cat $RESULT_JSON | jq -r '.targets[] | select( .executableLines > 0 ) | .lineCoverage' | sed -n '1p')
+# echo "Conversão concluída. O arquivo LCOV foi salvo como: $lcov_file"
+# #Filtrando para que a variavel code coverage apenas contenha a cobertura total
+# CODE_COVERAGE=$(cat $RESULT_JSON | jq -r '.targets[] | select( .executableLines > 0 ) | .lineCoverage' | sed -n '1p')
 
-# Multiplicando por cem para que o valor seja uma porcentagem
-CODE_COVERAGE=$(echo $CODE_COVERAGE*100.0 | bc)
+# # Multiplicando por cem para que o valor seja uma porcentagem
+# CODE_COVERAGE=$(echo $CODE_COVERAGE*100.0 | bc)
 
-# Verificando se a porcentagem obtida está de acordo com o esperado
-COVERAGE_PASSES=$(echo "$CODE_COVERAGE > $MIN_CODE_COVERAGE" | bc)
-if [ $COVERAGE_PASSES -ne 1 ]; then
-	printf "\033[0;31mCode coverage %.1f%% is less than required %.1f%%\033[0m\n" $CODE_COVERAGE $MIN_CODE_COVERAGE
-	exit -1
-else
-	printf "\033[0;32mCode coverage is %.1f%%\033[0m\n" $CODE_COVERAGE
-fi
+# # Verificando se a porcentagem obtida está de acordo com o esperado
+# COVERAGE_PASSES=$(echo "$CODE_COVERAGE > $MIN_CODE_COVERAGE" | bc)
+# if [ $COVERAGE_PASSES -ne 1 ]; then
+# 	printf "\033[0;31mCode coverage %.1f%% is less than required %.1f%%\033[0m\n" $CODE_COVERAGE $MIN_CODE_COVERAGE
+# 	exit -1
+# else
+# 	printf "\033[0;32mCode coverage is %.1f%%\033[0m\n" $CODE_COVERAGE
+# fi
