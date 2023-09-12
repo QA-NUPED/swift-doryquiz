8fd89c79f1666167b35c37249475bc96bb21567c
jhonyso <jhonysds2@gmail.com>
2023-09-12 14:41:44 -0300

    test

--- Scripts/code-coverage.sh
+++ Scripts/code-coverage.sh
@@ -2,7 +2,7 @@
 SCHEME="IndexedDataStore"
 RESULT_BUNDLE="CodeCoverage.xcresult"
 RESULT_JSON="CodeCoverage.json"
-
+GITHUB_API_URL="https://api.github.com/QA-NUPED/swift-doryquiz/pulls/7/comments"
 
 # Pre-clean
 if [ -d $RESULT_BUNDLE ]; then
@@ -13,6 +13,24 @@ fi
 set -o pipefail && env NSUnbufferedIO=YES xcodebuild build-for-testing -project "doryQuiz/doryQuiz.xcodeproj" -scheme "doryQuizTests" -destination "platform=iOS Simulator,OS=latest,name=iPhone 14" -enableCodeCoverage YES | xcpretty
 # Test
 set -o pipefail && env NSUnbufferedIO=YES xcodebuild test-without-building -project "doryQuiz/doryQuiz.xcodeproj" -scheme "doryQuizTests" -destination "platform=iOS Simulator,OS=latest,name=iPhone 14" -enableCodeCoverage YES -resultBundlePath $RESULT_BUNDLE | xcpretty
+
+# Verifique se os testes foram bem-sucedidos
+if [ $RESULT_BUNDLE -eq 0 ]; then
+  # Se os testes foram bem-sucedidos, crie um relatório a partir do arquivo de saída
+  echo "Testes foram bem-sucedidos. Relatório de Teste:" > comment_body.txt
+  cat "$RESULT_BUNDLE" >> comment_body.txt
+else
+  # Se os testes falharam, crie um relatório de falha
+  echo "Testes falharam. Relatório de Falha:" > comment_body.txt
+  cat "$RESULT_BUNDLE" >> comment_body.txt
+fi
+
+# Crie um comentário no GitHub no pull request
+curl -X POST -H "Authorization: token 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" \
+  -H "Content-Type: application/json" \
+  -d "{\"body\":\"$(cat comment_body.txt | jq -R -s -c '.[]')\"}" \
+  "$GITHUB_API_URL"
+
 # #Report
 # set -o pipefail && env NSUnbufferedIO=YES xcrun xccov view --report --json $RESULT_BUNDLE > $RESULT_JSON
 
