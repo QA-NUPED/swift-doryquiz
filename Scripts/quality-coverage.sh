943d4e000d751d850f5c7e7e5ba453265f92df57
jhonyso <jhonysds2@gmail.com>
2023-09-12 15:42:46 -0300

    test

--- Scripts/quality-coverage.sh
+++ Scripts/quality-coverage.sh
@@ -1,19 +1,50 @@
-RESULT_JSON="CodeCoverage.xcresult"
+#!/bin/bash
+SCHEME="IndexedDataStore"
+RESULT_BUNDLE="CodeCoverage.xcresult"
+RESULT_JSON="CodeCoverage.json"
 MIN_CODE_COVERAGE=60.0
 
+# Pre-clean
+if [ -d $RESULT_BUNDLE ]; then
+    rm -rf $RESULT_BUNDLE
+fi
 if [ -f $RESULT_JSON ]; then
-	rm $RESULT_JSON
+    rm $RESULT_JSON
 fi
 
-CODE_COVERAGE=$(cat $RESULT_JSON | jq -r '.targets[] | select( .executableLines > 0 ) | .lineCoverage' | sed -n '1p')
+# Build
+set -o pipefail && env NSUnbufferedIO=YES xcodebuild build-for-testing -project "doryQuiz/doryQuiz.xcodeproj" -scheme "doryQuizTests" -destination "platform=iOS Simulator,OS=latest,name=iPhone 14" -enableCodeCoverage YES | xcpretty
 
-# Multiplicando por cem para que o valor seja uma porcentagem
-CODE_COVERAGE=$(echo $CODE_COVERAGE*100.0 | bc)
+# Test
+set -o pipefail && env NSUnbufferedIO=YES xcodebuild test-without-building -project "doryQuiz/doryQuiz.xcodeproj" -scheme "doryQuizTests" -destination "platform=iOS Simulator,OS=latest,name=iPhone 14" -enableCodeCoverage YES -resultBundlePath $RESULT_BUNDLE | xcpretty
 
-# Verificando se a porcentagem obtida está de acordo com o esperado
-COVERAGE_PASSES=$(echo "$CODE_COVERAGE > $MIN_CODE_COVERAGE" | bc)
-if [ $COVERAGE_PASSES -ne 1 ]; then
-	printf "\033[0;31mCode coverage %.1f%% is less than required %.1f%%\033[0m\n" $CODE_COVERAGE $MIN_CODE_COVERAGE
+# Report
+set -o pipefail && env NSUnbufferedIO=YES xcrun xccov view --report --json $RESULT_BUNDLE > $RESULT_JSON
+
+# Parse JSON report and create coverage summary
+COVERAGE_SUMMARY=$(cat $RESULT_JSON | jq -r '
+    .targets[] | 
+    select(.executableLines > 0) | 
+    {
+        folder: .name, 
+        lineCoverage: .lineCoverage
+    }
+')
+
+# Comment on a random pull request
+PR_NUMBER=$(gh pr list | awk '{print $1}' | shuf -n 1)
+PR_COMMENT="Code Coverage Summary:\n$COVERAGE_SUMMARY"
+
+gh pr comment $PR_NUMBER --body "$PR_COMMENT"
+
+# Filtering code coverage
+CODE_COVERAGE=$(echo "$COVERAGE_SUMMARY" | jq -s 'map(.lineCoverage) | add / length * 100')
+
+# Check if code coverage meets the minimum requirement
+COVERAGE_PASSES=$(echo "$CODE_COVERAGE >= $MIN_CODE_COVERAGE" | bc)
+
+if [ $COVERAGE_PASSES -eq 1 ]; then
+    printf "\033[0;32mCode coverage is %.1f%%\033[0m\n" $CODE_COVERAGE
 else
-	printf "\033[0;32mCode coverage is %.1f%%\033[0m\n" $CODE_COVERAGE
+    printf "\033[0;31mCode coverage %.1f%% is less than required %.1f%%\033[0m\n" $CODE_COVERAGE $MIN_CODE_COVERAGE
 fi
