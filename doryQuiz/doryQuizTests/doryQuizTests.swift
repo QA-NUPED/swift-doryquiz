5427cc1878adad4b25d75b68c6776ca554cbd5fe
Gab <70073015+OmnGabriel@users.noreply.github.com>
2024-01-24 14:58:38 -0300

    Update doryQuizTests.swift

--- doryQuiz/doryQuizTests/doryQuizTests.swift
+++ doryQuiz/doryQuizTests/doryQuizTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class doryQuizTests: XCTestCase {
 
-    override func AAAAAsetUpWithError() throws {
+    override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
